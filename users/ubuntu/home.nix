482b1bee8772ef8aaf4456f3a00a211c0f00ea4c
Michael Kim <mike@michaelkim.net>
2025-06-30 19:04:47 +0000

    user: ubuntu

---
+++ users/ubuntu/home.nix
@@ -0,0 +1,32 @@
+{
+  pkgs,
+  ...
+}:
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "ubuntu";
+  home.homeDirectory = "/home/ubuntu";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  imports = [
+    ../../home/core.nix
+    ../../home/shell
+    ../../home/programs/helix
+    ../../home/programs/git
+  ];
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+  ];
+
+}
