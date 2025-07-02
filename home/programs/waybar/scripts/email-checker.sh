1519511a3997d0eb9e8deb57ebc7323a3f2e70e5
Michael Kim <mike@michaelkim.net>
2025-07-01 21:56:00 -0600

    email notification

--- home/programs/waybar/scripts/email-checker.sh
+++ home/programs/waybar/scripts/email-checker.sh
@@ -18,15 +18,15 @@ total_unread=$((personal_unread + tsbot_unread))
 
 # Format output for waybar
 if [[ $total_unread -gt 0 ]]; then
-    tooltip="$total_unread unread emails"
+    tooltip="$total_unread Inbox emails"
     if [[ $personal_unread -gt 0 && $tsbot_unread -gt 0 ]]; then
         tooltip="Personal: $personal_unread, TSBot: $tsbot_unread"
     elif [[ $personal_unread -gt 0 ]]; then
-        tooltip="Personal: $personal_unread unread"
+        tooltip="Personal: $personal_unread Inbox emails"
     elif [[ $tsbot_unread -gt 0 ]]; then
-        tooltip="TSBot: $tsbot_unread unread"
+        tooltip="TSBot: $tsbot_unread Inbox emails"
     fi
     echo "{\"text\": \"$total_unread\", \"tooltip\": \"$tooltip\", \"class\": \"unread\"}"
 else
-    echo "{\"text\": \"0\", \"tooltip\": \"No unread emails\", \"class\": \"read\"}"
+    echo "{\"text\": \"0\", \"tooltip\": \"No Inbox emails\", \"class\": \"read\"}"
 fi
