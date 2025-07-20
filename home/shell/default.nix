f8a538a2c49e66e49facccb49a9a8db6dabfb019
Michael Kim <mike@michaelkim.net>
2025-07-20 17:50:24 -0600

    atuin sync interval

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -179,6 +179,7 @@ in {
       show_preview = true;
       max_preview_height = 4;
       sync_address = "http://michaelkim.net:8888";
+      sync_frequency = "5m";
       history_filter = [
         "^secret-tool"
         "^pass"
