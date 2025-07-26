7a4f9bbb6cbb0a686ca67d7efe58f12abc5c9d46
Michael Kim <mike@michaelkim.net>
2025-07-26 08:10:07 -0600

    fix: yazi in hx

--- home/programs/helix/default.nix
+++ home/programs/helix/default.nix
@@ -78,8 +78,10 @@
           H = ":bp";
           L = ":bn";
           D = ["ensure_selections_forward" "extend_to_line_end"];
-          C-y = ":sh zellij run -f -x 10% -y 10% --width 80% --height 80% -- bash ~/.config/helix/yazi-picker.sh";
-          C-e = ":sh zellij run -f -x 10% -y 10% --width 80% --height 80% -- bash ~/.config/helix/yazi-picker.sh";
+          # C-y = ":sh zellij run -f -x 10% -y 10% --width 80% --height 80% -- bash ~/.config/helix/yazi-picker.sh";
+          # C-e = ":sh zellij run -f -x 10% -y 10% --width 80% --height 80% -- bash ~/.config/helix/yazi-picker.sh";
+          C-y = ":sh zellij run -f -x 10%% -y 10%% --width 80%% --height 80%% -- bash ~/.config/helix/yazi-picker.sh";
+          C-e = ":sh zellij run -f -x 10%% -y 10%% --width 80%% --height 80%% -- bash ~/.config/helix/yazi-picker.sh";
           Z = {
             Z = ":x";
           };
