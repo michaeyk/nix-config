c6e69ae4160d5a0d0debc0ab82841f530a10e139
Michael Kim <mike@michaelkim.net>
2025-08-02 15:39:17 -0600

    build: Justfile gaming/babysnacks

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -92,11 +92,6 @@ in {
       package = lib.mkDefault pkgs.jetbrains-mono;
       size = lib.mkDefault 10;
     };
-    # font = {
-    #   name = "Fira Code";
-    #   package = pkgs.fira-code;
-    #   size = 10;
-    # };
     settings = {
       enable_audio_bell = false;
       background = lib.mkDefault "#282828";
@@ -111,18 +106,7 @@ in {
       general.framerate = 60;
       # input.method = "alsa";
       smoothing.noise_reduction = 88;
-      color = {
-        gradient = 1;
-
-        gradient_color_1 = "'#94e2d5'";
-        gradient_color_2 = "'#89dceb'";
-        gradient_color_3 = "'#74c7ec'";
-        gradient_color_4 = "'#89b4fa'";
-        gradient_color_5 = "'#cba6f7'";
-        gradient_color_6 = "'#f5c2e7'";
-        gradient_color_7 = "'#eba0ac'";
-        gradient_color_8 = "'#f38ba8'";
-      };
+      
     };
   };
 
