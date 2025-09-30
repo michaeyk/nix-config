002ffc4fb6d703cf9b9559a8e0addfb218b61cd0
Michael Kim <mike@michaelkim.net>
2025-09-29 19:21:21 -0600

    upload screenshots to 0x0.st

--- home/shell/bin/screenshot.sh
+++ home/shell/bin/screenshot.sh
@@ -2,7 +2,22 @@
 
 cp /tmp/screenshot.png $HOME/Pictures/screenshots/screenshot_from_$(date +%Y-%m-%d_%H-%M-%S).png
 
-xbackbone_uploader.sh /tmp/screenshot.png 
+# Upload to 0x0.st and capture the URL
+url=$(curl -s -F'file=@/tmp/screenshot.png' https://0x0.st)
+
+if [ -n "$url" ]; then
+    # Copy URL to clipboard without newline
+    printf "%s" "$url" | wl-copy
+
+    # Send notification
+    notify-send "Screenshot uploaded" "Link copied to clipboard: $url" -i camera-photo
+
+    # Display the URL
+    echo "$url"
+else
+    notify-send "Screenshot upload failed" "Unable to upload to 0x0.st" -i dialog-error
+    echo "Upload failed"
+fi
 
 
 
