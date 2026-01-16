153787242cf3c596328a6b561544e5f82e6dea49
Michael Kim <mike@michaelkim.net>
2026-01-16 13:37:13 -0700

    Update random-wallpaper.sh for new hyprpaper IPC syntax

--- home/shell/bin/random-wallpaper.sh
+++ home/shell/bin/random-wallpaper.sh
@@ -1,11 +1,10 @@
 #!/bin/env bash
 
 WALLPAPER_DIR="$HOME/Pictures/wallpaper/"
-CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 # Get the name of the focused monitor with hyprctl
 FOCUSED_MONITOR=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
-# Get a random wallpaper that is not the current one
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+# Get a random wallpaper
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
 
-# Apply the selected wallpaper
-hyprctl hyprpaper reload "$FOCUSED_MONITOR","$WALLPAPER"
+# Apply the selected wallpaper using new hyprpaper IPC syntax
+hyprctl hyprpaper wallpaper "$FOCUSED_MONITOR, $WALLPAPER"
