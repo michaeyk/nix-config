f6d00e00231ddebb3adc1ac86ce1487592082b3c
Michael Kim <mike@michaelkim.net>
2026-02-11 18:12:48 -0700

    Enable Wake-on-LAN with magic packet on gaming host

--- hosts/gaming/configuration.nix
+++ hosts/gaming/configuration.nix
@@ -392,7 +392,21 @@ in {
     yubikey-manager
     pam_u2f
     davfs2
+    ethtool
   ];
+
+  # Wake-on-LAN: enable magic packet wake on ethernet interface
+  systemd.services.wol-enable = {
+    description = "Enable Wake-on-LAN on enp4s0";
+    after = [ "network-pre.target" ];
+    wants = [ "network-pre.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.ethtool}/bin/ethtool -s enp4s0 wol g";
+      RemainAfterExit = true;
+    };
+  };
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
