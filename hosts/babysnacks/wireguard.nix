5c5e31015af72bff9c6917ac8751644b2e7f2579
Michael Kim <mike@michaelkim.net>
2026-07-13 19:21:15 -0600

    Keep only wg0 wireguard interface on babysnacks and gaming
    Remove the ai-workstation (wg1/wg2) and linode/Augusta (wg1) tunnels,
    leaving just the dellbro00 wg0 peer on each host.

--- hosts/babysnacks/wireguard.nix
+++ hosts/babysnacks/wireguard.nix
@@ -1,5 +1,5 @@
 { lib, ... }: {
-  systemd.services = lib.genAttrs ["wg-quick-wg0" "wg-quick-wg1"] (_: {
+  systemd.services = lib.genAttrs ["wg-quick-wg0"] (_: {
     serviceConfig = {
       TimeoutStartSec = "15s";
       TimeoutStopSec = "15s";
@@ -22,24 +22,6 @@
         publicKey = "FjqISjUvlqYIjYxWHO4K4RNfo+O//qaGiOEInXzJjBY=";
         allowedIPs = [ "10.253.0.1/32" "172.16.0.142/32"];
         endpoint = "${server_ip}:51820";
-        persistentKeepalive = 25;
-      }];
-    };
-
-    # ai-workstation
-    wg1 = {
-      # IP address of this machine in the *tunnel network*
-      address = [
-        "10.8.0.2/32"
-      ];
-
-      # Path to the private key file.
-      privateKeyFile = "/home/mike/.wireguard/private-key";
-
-      peers = [{
-        publicKey = "Uj/Mnp8xqpnLMRUAwexG1p2PF8CKLz4K6MhOOqscBxE=";
-        allowedIPs = [ "10.8.0.1/32" "172.16.0.135/32"];
-        endpoint = "${server_ip}:52820";
         persistentKeepalive = 25;
       }];
     };
--- hosts/gaming/wireguard.nix
+++ hosts/gaming/wireguard.nix
@@ -1,5 +1,5 @@
 {lib, ...}: {
-  systemd.services = lib.genAttrs ["wg-quick-wg0" "wg-quick-wg1" "wg-quick-wg2"] (_: {
+  systemd.services = lib.genAttrs ["wg-quick-wg0"] (_: {
     serviceConfig = {
       TimeoutStartSec = "15s";
       TimeoutStopSec = "15s";
@@ -23,46 +23,6 @@
           publicKey = "FjqISjUvlqYIjYxWHO4K4RNfo+O//qaGiOEInXzJjBY=";
           allowedIPs = ["10.253.0.1/32" "172.16.0.142/32"];
           endpoint = "${server_ip}:51820";
-          persistentKeepalive = 25;
-        }
-      ];
-    };
-
-    # linode to access Augusta
-    wg1 = {
-      # IP address of this machine in the *tunnel network*
-      address = [
-        "10.10.0.5/32"
-      ];
-
-      # Path to the private key file.
-      privateKeyFile = "/home/mike/.wireguard/private-key";
-
-      peers = [
-        {
-          publicKey = "ro+c6m1V+p5tLioNdPiIQsPrVn0DOsydJmdu83ljG1E=";
-          allowedIPs = ["10.10.0.0/24"];
-          endpoint = "172.235.56.182:51820";
-          persistentKeepalive = 25;
-        }
-      ];
-    };
-
-    # ai-workstation
-    wg2 = {
-      # IP address of this machine in the *tunnel network*
-      address = [
-        "10.8.0.9/32"
-      ];
-
-      # Path to the private key file.
-      privateKeyFile = "/home/mike/.wireguard/private-key";
-
-      peers = [
-        {
-          publicKey = "Uj/Mnp8xqpnLMRUAwexG1p2PF8CKLz4K6MhOOqscBxE=";
-          allowedIPs = ["10.8.0.1/32" "172.16.0.135/32"];
-          endpoint = "${server_ip}:52820";
           persistentKeepalive = 25;
         }
       ];
