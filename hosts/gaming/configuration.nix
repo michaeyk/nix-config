5296687e7cf7da60a3cbd222f2aaa1352aed04d7
Michael Kim <mike@michaelkim.net>
2026-07-18 20:26:04 -0600

    Add CIFS/Samba mount of share_media over WireGuard on gaming
    Mirror the babysnacks /mnt/media mount onto gaming: //10.253.0.1/share_media
    via on-demand automount, sops-encrypted credentials (smb_dellbro00, shared
    secrets.yaml), cifs-utils, ordered after wg-quick-wg0.

--- hosts/gaming/configuration.nix
+++ hosts/gaming/configuration.nix
@@ -78,6 +78,16 @@ in {
       path = "/etc/davfs2/secrets";
     };
 
+    # CIFS credentials file for //10.253.0.1/share_media.
+    # Read by root-run mount.cifs at automount time; owner=root, mode 0400.
+    # Decrypted content must be a mount.cifs credentials file, e.g.:
+    #   username=youruser
+    #   password=yourpass
+    "smb_dellbro00" = {
+      owner = "root";
+      mode = "0400";
+    };
+
     picoshare_token = {
       owner = config.users.users.mike.name;
       mode = "0400";
@@ -523,6 +533,7 @@ in {
     yubikey-manager
     pam_u2f
     davfs2
+    cifs-utils # mount.cifs helper for the //10.253.0.1/share_media SMB mount
     picocom # serial console for Dell MD1200 EMM diagnostics
     lrzsz # sx: plain XMODEM for EMM firmware flashing
     imsprog # CH341A EEPROM programmer GUI
@@ -566,6 +577,28 @@ in {
         "x-systemd.idle-timeout=600"
         "x-systemd.mount-timeout=10s"
         "x-systemd.device-timeout=5s"
+      ];
+    };
+
+    "/mnt/media" = {
+      device = "//10.253.0.1/share_media";
+      fsType = "cifs";
+      options = [
+        "credentials=${config.sops.secrets.smb_dellbro00.path}"
+        "uid=1000"
+        "gid=100"
+        "iocharset=utf8"
+        "file_mode=0664"
+        "dir_mode=0775"
+        "noauto"
+        "nofail"
+        "_netdev"
+        "x-systemd.automount"
+        "x-systemd.idle-timeout=600"
+        "x-systemd.mount-timeout=10s"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.after=wg-quick-wg0.service"
+        "x-systemd.requires=wg-quick-wg0.service"
       ];
     };
   };
